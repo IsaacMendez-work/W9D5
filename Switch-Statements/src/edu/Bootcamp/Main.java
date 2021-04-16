ab667399583bcc16cea1f495b2d40ec4ac5604ce
Eyke Mendez <isaacmendez.work+gitbash@gmail.com>
2021-04-16 16:03:39 -0400

    first commit

---
+++ Switch-Statements/src/edu/Bootcamp/Main.java
@@ -0,0 +1,28 @@
+package edu.Bootcamp;
+
+public class Main {
+
+    public static void main(String[] args) {
+/*
+A switch is a way to implement conditional logic by testing a single value against multiple potential-matches.
+For example:
+      switch (value-to-test) {
+          case matching-value-1:
+              statements-to-do
+              break;
+          case matching-value-2:
+              other-statements
+              break;
+          case matching-value-N:
+              more-statements
+              break;
+          default:
+              statements
+      }
+NOTE: default is optional.
+NOTE: break is optional, but recommended if you don't want to run every following case until the end of the switch statement.
+
+Only integer and char primitive data types can be used with a switch.
+ */
+    }
+}
